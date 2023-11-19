2fef7f1717ea63c1a308f7f52d8c4eee6e3c6a4e
Geladen <cuncuemanuele@outlook.it>
2023-11-19 12:14:26 +0100

    update

--- src/bb84.py
+++ src/bb84.py
@@ -1,35 +1,67 @@
-from qiskit import QuantumCircuit, Aer
+from qiskit import QuantumCircuit, Aer, execute
 import numpy as np
 import Crypto.Random.random
+from qiskit.providers.aer import QasmSimulator
+
+SIZE = 1024
 
 # Encode each bit according to the chosen base
-def encode(qc, bit, base):
+def encode(bits, bases):
 
-    if base == 'H':
-        if bit == 1:
-            qc.x(0)
-    else:
-        if bit == 1:
-            qc.h(0)
-            qc.x(0)
+    encoded_bits = []
+
+    for i in range(SIZE):
+        qc = QuantumCircuit(1,1)
+        if bases[i] == 0:
+            if bits[i] == 0:
+                pass 
+            else:
+                qc.x(0)
         else:
-            qc.h(0)
-    return qc
+            if bits[i] == 0:
+                qc.h(0)
+            else:
+                qc.x(0)
+                qc.h(0)
+
+        encoded_bits.append(qc)
+
+    return encoded_bits
+
+# Measure al qubits with the beses given by Bob
+def measure_bits(bits, bases):
+    measured_bits = []
+    backend = Aer.get_backend('aer_simulator')
+
+    for i in range(SIZE):
+        if bases[i] == 0: 
+            bits[i].measure(0,0)
+        if bases[i] == 1: 
+            bits[i].h(0)
+            bits[i].measure(0,0)
+        
+        result = backend.run(bits[i], shots=1, memory=True).result()
+        measured_bit = int(result.get_memory()[0])
+        measured_bits.append(measured_bit)
+    
+    return measured_bits
+
 
 if __name__ == '__main__':
 
     # Alice generates random bits
-    Alice_bits = Crypto.Random.random.getrandbits(1024)
+    Alice_bits = [int(d) for d in bin(Crypto.Random.random.getrandbits(SIZE))[2:].rjust(SIZE, '0')] 
+
     # Alice generates random bits in order to choose bases
-    Alice_bases = Crypto.Random.random.getrandbits(1024)
-    print(bin(Alice_bits))
-    print(bin(Alice_bases))
+    Alice_bases = [int(d) for d in bin(Crypto.Random.random.getrandbits(SIZE))[2:].rjust(SIZE, '0')] 
 
-    # Create a circuit
-    qc = QuantumCircuit(1,1)
-    qc.measure(0,0)
+    # Alice applies her bases
+    qc_list = encode(Alice_bits, Alice_bases)
 
-    # Simulate circuit
-    aer_sim = Aer.get_backend('aer_simulator')
-    job = aer_sim.run(qc)
-    print(job.result().get_counts())
+    # Bob generates random bits in order to choose bases
+    Bob_bases = [int(d) for d in bin(Crypto.Random.random.getrandbits(SIZE))[2:].rjust(SIZE, '0')] 
+
+    # Bob measures all the bits
+    measured_bits = measure_bits(qc_list, Bob_bases)
+
+    print(measured_bits)
